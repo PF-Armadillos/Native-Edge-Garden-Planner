e0890a2c1f0d8b6b9343a4e28e7ba4e5b154b2c3
mayagoyle <mayagoyle99@gmail.com>
2024-04-27 14:40:29 -0400

    react setup

---
+++ client/app.jsx
@@ -0,0 +1,4 @@
+import React, {useState} from "react";
+import { Switch, Route } from 'react-router-dom';
+
+
--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
-import './../styles.css';
+import '../styles.css';
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello Pink  Fairy Armadillos!';
+  return element;
+}
+document.body.appendChild(component());
