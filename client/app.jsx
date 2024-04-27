50e97c70b69ad7110dd1106cad5398586f9e0b98
mayagoyle <mayagoyle99@gmail.com>
2024-04-27 15:41:51 -0400

    welcome page react setup

--- client/app.jsx
+++ client/app.jsx
@@ -1,4 +1,61 @@
 import React, {useState} from "react";
 import { Switch, Route } from 'react-router-dom';
 
+const App = () => {
 
+    const [location, setLocation] = useState('');
+    const [length, setLength] = useState('');
+    const [width, setWidth] = useState('');
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+        console.log("Location:", location);
+        console.log("Length:", length);
+        console.log("Width:", width);
+        
+    }
+    return(
+        <div>
+            <h1>Welcome to the ShellScape Garden Planner</h1>
+            <h2>Complete the form below to get started!</h2>
+
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Location</label>
+                <input 
+                    type="text" 
+                    id="location"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                >
+                </input>
+              </div>
+              <div>
+                <label>Length of garden bed (inches)</label>
+                <input 
+                    type="text" 
+                    id="length"
+                    value={length}
+                    onChange={(e) => setLength(e.target.value)}
+                >
+                </input>
+              </div>
+              <div>
+                <label>Width of garden bed (inches)</label>
+                <input 
+                    type="text" 
+                    id="width"
+                    value={width}
+                    onChange={(e) => setWidth(e.target.value)}
+                >
+                </input>
+              </div>
+              <button type="submit"> Submit</button>
+            </form>
+        </div>
+    )
+
+
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,7 +1,18 @@
 import '../styles.css';
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello Pink  Fairy Armadillos!';
-  return element;
-}
-document.body.appendChild(component());
+import App from './app.jsx';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// function component() {
+//   const element = document.createElement('div');
+//   element.innerHTML = 'Hello Pink  Fairy Armadillos!';
+//   return element;
+// }
+// document.body.appendChild(component());
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+,);
