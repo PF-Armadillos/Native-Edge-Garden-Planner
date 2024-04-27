4ac3746752a35cc0847e9ee66390696fe8b3d474
aissata <nutzela@gmail.com>
2024-04-27 11:28:39 -0400

    added boiler plate to index.js and server.js and modified webpack

--- client/index.js
+++ client/index.js
@@ -1,1 +1,8 @@
-import './../styles.css';
+import '../styles.css';
+import '../styles.css';
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello Pink  Fairy Armadillos!';
+  return element;
+}
+document.body.appendChild(component());
