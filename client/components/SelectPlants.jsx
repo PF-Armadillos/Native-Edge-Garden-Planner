59574cd3b0a54bab60f829b42f86275071504cf8
Michael Ma <122073103+michaelma7@users.noreply.github.com>
2024-04-29 18:04:29 -0400

    Update SelectPlants.jsx
    Changed so I don't lose progress to test elements
    
    Signed-off-by: Michael Ma <122073103+michaelma7@users.noreply.github.com>

--- client/components/SelectPlants.jsx
+++ client/components/SelectPlants.jsx
@@ -4,63 +4,41 @@ import plantDatabase from '../staticObject.js';
 import { useSelector } from 'react-redux';
 
 export default function SelectPlants() {
-//   const sampleTest = {
-//     _id: '662fbb7313277adf58aeef05',
-//     State: 'New York',
-//     Species: 'Acer rubrum',
-//     CommonName: 'Red Maple',
-//     Duration: 'Perennial',
-//     Habit: 'Tree',
-//     Light: 'Sun, Part-shade',
-//     Water: 'Moist',
-//     Thumb: 'https://shellscape.s3.us-east-2.amazonaws.com/A_C_IMG2564.JPG',
-//   };
-
-
-  //   "_id": "662fbb7313277adf58aeef0b",
-  //   "State": "New York",
-  //   "Species": "Adiantum pedatum",
-  //   "CommonName": "Northern Maidenhair Fern",
-  //   "Duration": "Perennial",
-  //   "Habit": "Fern",
-  //   "Light": "Shade, Part-shade",
-  //   "Water": "Moist",
-  //   "Thumb": "https://shellscape.s3.us-east-2.amazonaws.com/A_C_IMG3197.JPG"
-  // },
-  // const { CommonName, Habit, Duration, Thumb, _id } = sampleTest;
-  // const testarr= [    <PlantCard
-  //   commonName={CommonName}
-  //   habit={Habit}
-  //   duration={Duration}
-  //   image={Thumb}
-  //   plantId={_id}
-  // />,
-  // <PlantCard
-  //   commonName={"Northern Maidenhair Fern"}
-  //   habit={Habit}
-  //   duration={Duration}
-  //   image={Thumb}
-  //   plantId={_id}
-  // />,
-  // <PlantCard
-  //     commonName={CommonName}
-  //     habit={Habit}
-  //     duration={Duration}
-  //     image={Thumb}
-  //     plantId={_id}
-  //   />,
-  // <PlantCard
-  //     commonName={CommonName}
-  //     habit={Habit}
-  //     duration={Duration}
-  //     image={Thumb}
-  //     plantId={_id}
-  //   />]
-  // return (
-  //   <>
-  //     <div className='PlantContainer'>
-  //       {testarr}
-  //     </div>
-  //   </>
-  // );
-}
+  //fetch all plants from database via url
+  //for loop nonsense here
+  const plantData = useSelector((state) => state.plants.plantList);
+  console.log(plantData);
+  const array = [];
+  for (const ele of plantData) {
+    array.push(
+      <PlantCardSelect
+        commonName={ele.CommonName}
+        species={ele.Species}
+        habit={ele.Habit}
+        duration={ele.Duration}
+        image={ele.Thumb}
+        light={ele.Light}
+        water={ele.Water}
+        plantId={ele._id}
+        key={ele._id}
+      />
+    );
+  }
+  return (
+    <div className='plant-list'>
+      {array}
+      {/* {plantDatabase.map((plant) => {
+        <PlantCardSelect
+          commonName={plant.CommonName}
+          species={plant.Species}
+          duration={plant.Duration}
+          habit={plant.Habit}
+          image={plant.Thumb}
+          light={plant.Light}
+          water={plant.Water}
+          plantId={plant._id}
+          key={plant._id}
+        />;
+      })} */}
+    </div>
+  );
