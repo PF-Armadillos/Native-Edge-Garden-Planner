207e03213f72741cf21f6866ae51436065d06177
aissata <nutzela@gmail.com>
2024-04-30 17:57:27 -0400

    1.0 first edits

---
+++ tests/db.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const db = require('../server/controllers/userModel.js');
+
+// describe('sample test', () => {
+//   it('test', () => {
+    
+//     const result = db.query();
+//     expect(result).toBeInstanceOf(Error);
+   
+//   });
+
+  
+// });
